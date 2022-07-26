b4f9255fb6d4c24498039603163dee1f821d7274
Svuara <robertoencina@gmail.com>
2022-07-26 20:11:24 -0300

    cambios

--- app/src/main/java/com/example/kotlinparaprincipiantes/MainActivity.kt
+++ app/src/main/java/com/example/kotlinparaprincipiantes/MainActivity.kt
@@ -9,48 +9,51 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        variablesYConstantes()
+    }
 
 
+    private fun variablesYConstantes() {
 
-        private fun variablesYConstantes() {
+        // Variables
 
-            // Variables
+        var miPrimerVariable = " hola Robertito "
+        println(miPrimerVariable)
 
-            var miPrimerVariable = " hola Robertito "
-            println(miPrimerVariable)
-
-            miPrimerVariable = "Bienvenidos"
-            println(miPrimerVariable)
-            var miSegundaVariable = "suscribete"
-            println(miSegundaVariable)
-            miSegundaVariable = miPrimerVariable
-            println(miSegundaVariable)
+        miPrimerVariable = "Bienvenidos"
+        println(miPrimerVariable)
+        var miSegundaVariable = "suscribete"
+        println(miSegundaVariable)
+        miSegundaVariable = miPrimerVariable
+        println(miSegundaVariable)
 // constantes
-            val miPrimerCosntante = "te gusta el tuto"
-            println(miPrimerCosntante)
+        val miPrimerCosntante = "te gusta el tuto"
+        println(miPrimerCosntante)
 
-        }
-        /* tipos de datos
-        */
-        // string cadena de tecxto
+    }
+    /* tipos de datos
+    */
+    // string cadena de tecxto
 
 
-        private fun tiposDeDatos() {
+    private fun tiposDeDatos() {
 
-            val myString: String = "Hola Robertito"
-            val myString2 = "Bienvenidos"
-            val myString3 = myString + " " + myString2
-            println(myString3)
+        val myString: String = "Hola Robertito"
+        val myString2 = "Bienvenidos"
+        val myString3 = myString + " " + myString2
+        println(myString3)
 
-            // enteros (Byte, Short, int, long)
+        // enteros (Byte, Short, int, long)
 
-            val myInt: Int = 1
-            val myInt2 = 2
-            val myInt3: Int = (myInt + myInt2)
-            println(myInt3)
-            // decimales (float double)
-            val myFloat: float = 1.5f
-            val myDouble = 1.5
+        val myInt: Int = 1
+        val myInt2 = 2
+        val myInt3: Int = (myInt + myInt2)
+        println(myInt3)
+        // decimales (float double)
+        val myFloat: Float = 1.5f
+        val myDouble = 1.5
 
-            }
+    }
 
+
+}
